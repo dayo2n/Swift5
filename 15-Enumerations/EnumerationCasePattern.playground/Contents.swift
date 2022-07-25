f8ef844d82e57a9fba7fe20bcde8deae643aef16
dayo2n <0217dayun@naver.com>
2022-07-25 15:16:35 +0900

    Section 15. Enumeration(2): Enumeration case pattern

--- 15-Enumerations/EnumerationCasePattern.playground/Contents.swift
+++ 15-Enumerations/EnumerationCasePattern.playground/Contents.swift
@@ -26,57 +26,65 @@ import UIKit
  ![enum-case](enum-case.png)
  */
 
+// 연관값을 가진 열거형 케이스를 매칭시킬 때 사용
+// 각종 statement에서 사용
+
+enum Transportation {
+    case bus(number: Int)
+    case taxi(company: String, number: String)
+    case subway(lineNumber: Int, express: Bool)
+}
+
+var tpt = Transportation.bus(number: 7)
+
+switch tpt {
+case .bus(let n): // 연관값을 n 상수에 바인딩
+    print(n)
+case .taxi(let c, var n): // 모든 연관값을 바인딩해야하는데, 필요없는 바인딩이라면 와일드카드(_) 이용
+    print(c, n)
+case let .subway(l, e): // 바인딩 방식이 let || var 중 let으로 동일하다면 한번에 바인딩 가능
+    print(l, e)
+}
+
+
+
+tpt = Transportation.subway(lineNumber: 2, express: false)
+
+if case let .subway(2, express) = tpt {
+    // 첫번째 연관값이 2로 바인딩되어있을 때 실행될 블록
+    if express {
+        
+    } else {
+        
+    }
+}
+
+// (와일드카드로 무시, 값을 직접 입력)했으므로 let 또는 var로 바인딩 X
+if case .subway(_, true) = tpt {
+    print("express")
+}
 
 
 
 
 
+let list = [
+    Transportation.subway(lineNumber: 2, express: false),
+    Transportation.bus(number: 4412),
+    Transportation.subway(lineNumber: 7, express: true),
+    Transportation.taxi(company: "SeoulTaxi", number: "1234")
+]
 
+for case let .subway(n, _) in list {
+    print("subway \(n)")
+}
 
+for case let .subway(n, true) in list {
+    print("subway \(n)")
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for case let .subway(n, true) in list where n == 2 { // where절은 collection 뒤에 정의 
+    print("subway \(n)")
+}
 
 
