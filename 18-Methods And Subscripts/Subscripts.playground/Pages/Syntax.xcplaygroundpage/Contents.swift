5ffe86bf3d8e98be2b28eb1bc87e834b7a53e971
dayo2n <0217dayun@naver.com>
2022-07-27 16:39:50 +0900

    Section 18. Method and Subscript(2) : Subscript

--- 18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
+++ 18-Methods And Subscripts/Subscripts.playground/Pages/Syntax.xcplaygroundpage/Contents.swift
@@ -27,26 +27,50 @@ import UIKit
  ![subscript](subscript.png)
  */
 
-
-
-
-
-
-
-
-
-
+let list = ["A", "B", "C"]
+list[0]
 
 /*:
  ![subscript-syntax](subscript-syntax.png)
  */
 
+// subscript는 최소한 값을 읽을 수는 있어야하므로 get없이 set블록만 있을 수 없음
+
+class List {
+    var data = [1, 2, 3]
+    
+    subscript(index: Int) -> Int {
+//        subscript(index: Int) -> Int {
+//          argument label 사용 가능 : 파라미터가 두 개 이상이거나 가독성을 높여주고싶을 때 사용, 보통은 사용하지 않음
+        get {
+            return data[index]
+        }
+        
+        set {
+            data[index] = newValue
+        }
+    }
+}
+
+var l = List()
+l[0]
+
+l[1] = 123
+
+// l[0, 1] // error!
+// l["A"] // error!
 
 
+struct Matrix {
+    var data = [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]]
+    
+    subscript(row: Int, col: Int) -> Int {
+        return data[row][col] // read-only (set block 생략)
+    }
+}
 
-
-
-
-
-
-
+let m = Matrix()
+m[0, 0] // param이 두 개 이상이면 콤마로 구분해 전달
+m[0, 10] // Index out of range -> TODO: if와 optional을 사용해 범위 검사
