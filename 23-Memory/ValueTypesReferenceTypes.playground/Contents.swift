08040a66b73250d37009dffe66a9a1b0f5693d48
dayo2n <0217dayun@naver.com>
2022-08-24 15:25:15 +0900

    Section 23. Memory(2): value type vs ref type

--- 23-Memory/ValueTypesReferenceTypes.playground/Contents.swift
+++ 23-Memory/ValueTypesReferenceTypes.playground/Contents.swift
@@ -25,40 +25,51 @@ import UIKit
  # Value Types & Reference Types
  */
 
-struct SizeValue {
+struct SizeValue { // 구조체는 값형식
    var width = 0.0
    var height = 0.0
 }
 
 var value = SizeValue()
+// 인스턴스를 생성하면 스택에 메모리공간이 생성되어 저장됨
 
 
-var value2 = value
+var value2 = value // 속성의 값을 복사해 새로운 메모리에 저장됨 -> value와 value2는 서로 다른 공간에 저장됨
 value2.width = 1.0
 value2.height = 2.0
 
 value
 value2
+// 인스턴스 속성의 값은 다름
 
 
-class SizeObject {
+class SizeObject { // 클래스는 참조형식
    var width = 0.0
    var height = 0.0
 }
 
 var object = SizeObject()
+// 스택과 힙에 새로운 공간이 생성된다. 힙에는 인스턴스가 저장되고, 스택에는 인스턴스가 가리키는 힙 메모리 주소가 저장된다.
 
-var object2 = object
+var object2 = object // 힙에서는 새로운 메모리공간이 생성되거나 접근하지는 않음. 스택에 새로운 메모리 공간이 생성된다.
 
+// 값을 변경할 땐 항상 스택을 거쳐 힙에서 변경된다.
 object2.width = 1.0
 object2.height = 2.0
 
 object
 object2
+// 두 개가 같은 주소(인스턴스)를 가리키므로 속성의 값들은 같음
 
 
 
+
+/*
+ # keyword let
+ */
 let v = SizeValue()
+// v.width = 1.0 // error! 메모리에 저장된 값을 바꿀 수 없음
+// 값 형식을 let 키워드로 선언하면 속성까지 상수로 선언된다.
 
 
 let o = SizeObject()
@@ -69,8 +80,11 @@ o.height = 2.0
 
 
 
-
-
+/*
+ # 비교 연산시
+ */
+// value type : ==, !=
+// Reference Type: ==, != / ===, !==
 
 
 
